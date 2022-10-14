43630188c16d96a07b9e434c27386354fc981b8a
cmckee120993 <103902212+cmckee120993@users.noreply.github.com>
2022-10-13 20:16:26 -0700

    group model created, members/admin tied to user

---
+++ Develop/models/Group.js
@@ -0,0 +1,43 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Group extends Model {}
+
+Group.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        group_name: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        group_admin: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        group_members: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'group',
+    }
+);
+
+module.exports = Group;
