0f19c41a9823db0a366ea1952e930a9d903ff792
WrittenInnaCode <writteninnacode@gmail.com>
2022-10-18 03:05:02 -0700

    added email in user model, dashboard routes; made signup, login & logout pages working

--- Develop/models/User.js
+++ Develop/models/User.js
@@ -20,6 +20,14 @@ username: {
     type: DataTypes.STRING,
     allowNull: false,
 },
+email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+        isEmail: true,
+    },
+},
 password: {
     type:DataTypes.STRING,
     allowNull: false,
