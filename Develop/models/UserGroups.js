49b644f0cf28e173634bd8eaedec6af453f33328
Clay McKee <cmckee120993@gmail.com>
2022-10-18 00:46:06 -0400

    updated usergroups to cross reference

--- Develop/models/UserGroups.js
+++ Develop/models/UserGroups.js
@@ -1,2 +1,37 @@
-// user ID and group ID 
-// as foreign IDs
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserGroups extends Model {}
+
+UserGroups.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        group_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'group',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key:'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'user_groups',
+    }
+);
+
+module.exports = UserGroups;
