107befb68840dbb79f0037f1a165c162c11dfb3a
WrittenInnaCode <writteninnacode@gmail.com>
2022-10-18 14:45:23 -0700

    added a modal for creating a new group

---
+++ Develop/public/js/groupsHandlers/modal.js
@@ -0,0 +1,18 @@
+const newGroupBttn = document.querySelector('#newGroupBttn');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.delete');
+
+
+newGroupBttn.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+console.log('click')
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+});
+
+closeModal.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+});
