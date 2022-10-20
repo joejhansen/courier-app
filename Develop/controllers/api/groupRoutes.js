d975e4a5e63fc2abb6814b7760d815672089d44d
Lulu Jovovich <lulujovovich@Lulus-MBP.lan>
2022-10-19 19:19:25 -0700

    new group routes

--- Develop/controllers/api/groupRoutes.js
+++ Develop/controllers/api/groupRoutes.js
@@ -1,6 +1,51 @@
 const router = require('express').Router();
 const { Group } = require('../../models');
 
+router.post('/', async (req, res) => {
+    const body = req.body;
 
+    try {
+        const newGroup = await Group.create({ ...body, user_id: req.session.user_id,})
+        res.json(newGroup);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+      const [affectedRows] = await Group.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (affectedRows > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const [affectedRows] = Group.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
