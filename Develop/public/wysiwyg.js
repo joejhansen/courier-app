820224873002fc243836b39f327a4efbee91618e
Clay McKee <cmckee120993@gmail.com>
2022-10-20 17:05:15 -0700

    cleaning of branch after things didn't work

--- Develop/public/wysiwyg.js
+++ Develop/public/wysiwyg.js
@@ -1,19 +1,10 @@
-// entire editor
 const editor = document.getElementById('.wysiwyg-form');
 const toolbar = document.getElementById('.toolbar');
 const buttons = document.querySelectorAll('.editor-btn');
-const contentArea = document.getElementById('.content-area');
+const contentArea = document.getElementById('.textarea');
 const modal = document.getElementById('.modal');
-const visuellView = document.getElementById('.textarea')
-
-document.addEventListener('selectionchange', selectionChange);
-
-// visuellView.addEventListener('paste', pasteEvent());
-// document.addEventListener('DOMContentLoaded', function(event){
-// contentArea.addEventListener("keyup", addParagraphTag);
-// });
-
-console.log(buttons);
+const visuellView = document.getElementById('.visuell-view');
+const postBtn = document.querySelector('.post-btn');
 
 for (let i = 0; i<buttons.length; i++) {
     let button = buttons[i];
@@ -21,111 +12,15 @@ for (let i = 0; i<buttons.length; i++) {
     button.addEventListener('click', function(e) {
         let action = this.dataset.action;
         console.log(button);
-
-        switch(action) {
-            case 'toggle-view':
-                execCodeAction(this, editor);
-                break;
-                case 'createLink':
-                    execLinkAction();
-                    break;
-                default:
-                    execDefaultAction(action);
-        }
-    });
-};
-
-function execCodeAction(button, editor) {
-    if(button.classList.contains('active')) {
-        visuellView.innerHTML = htmlView.value;
-        htmlView.style.display = 'none';
-        visuellView.style.display = 'block';
-
-        button.classList.remove('active');
-    } else {
-        htmlView.innerText = visuellView.innerHTML;
-        visuellView.style.display = 'none';
-        htmlView.style.dsiplay='block';
-
-        button.classList.add('active');
+        execDefaultAction(action);
+        })
     };
-};
 
-function execLinkAction(){
-    modal.style.display = 'block';
-    let selection = saveSelection();
-    
-    let submit = modal.querySelectorAll('button.done')[0];
-    let close = modal.querySelectorAll('.close')[0];
-
-    submit.addEventListener('click', function(e) {
-        e.preventDefault();
-        let newTabCheckbox = modal.querySelectorAll('#new-tab')[0];
-        let linkInput = modal.querySelectorAll('#linkValue')[0];
-        let linkValue = linkInput.value;
-        let newTab = newTabCheckbox.checked;
-
-        restoreSelection(selection);
-
-        if(window.getSelection().toString()) {
-            let a = document.createElement('a');
-            a.href=linkValue;
-            if(newTab) a.target = '_blank';
-            window.getSelection().getRangeAt(0).surroundContents(a);
-        }
-    
-        modal.style.display = 'none';
-        linkInput.value = '';
-
-        submit.removeEventListener('click', arguments.callee);
-        close.removeEventListener('click', arguments.callee);
-    });
-
-    close.addEventListener('click', function(e) {
-        e.preventDefault();
-        let linkInput = modal.querySelectorAll('#linkValue')[0];
-
-        modal.style.display = 'none';
-        linkInput.value = '';
-
-        submit.removeEventListener('click', arguments.callee);
-        close.removeEventListener('click', arguments.callee);
-    });
-};
 
 function execDefaultAction(action) {
     document.execCommand(action, false);
 };
 
-function saveSelection() {
-    if(window.getSelection) {
-        sel = window.getSelection();
-        if(sel.getRangeAt && sel.rangeCount) {
-            let ranges = [];
-            for(var i=0, len = sel.rangeCount; i<len; ++i) {
-                ranges.push(sel.getRangeAt(i));
-            }
-            return ranges;
-        }
-    } else if (document.selection && document.selection.createRange) {
-        return document.selection.createRange();
-    }
-    return null;
-};
-
-function restoreSelection(savedSel) {
-    if(savedSel) {
-        if(window.getSelection) {
-            sel = window.getSelection();
-            sel.removeAllRanges();
-            for(var i=0, len = savedSel.length; i<len; ++i) {
-                sel.addRange(savedSel[i]);
-            }
-        } else if (document.selection && savedSel.select) {
-            savedSel.select();
-        }
-    }
-};
 
 function selectionChange(e) {
 
@@ -151,34 +46,9 @@ function selectionChange(e) {
 });
 };
 
-
-
-function parentTagActive(elem) {
-    if(!elem || !elem.classList || elem.classList.contains('visuell-view')) return false;
-
-    let toolbarButton;
-    
-    let tagName = elem.tagName.toLowerCase();
-    toolbarButton = document.querySelectorAll(`.toolbar .editor-btn[data-tag-name="${tagName}"]`)[0];
-
-    if(toolbarButton) {
-        toolbarButton.classList.add('active')
-        }
-        return
-        parentTagActive(elem.parentNode);
-};
-
-function pasteEvent(e) {
-    e.preventDefault();
-    
-    let text = (e.originalEvent || e.clipboardData.getData('text/plain'));
-
-    document.execCommand('insertHTML', false, text);
-};
-
-// function addParagraphTag(evt) {
-//     if (evt.keyCode == '13') {
-//         if(window.getSelection().anchorNode.parentNode.tagName === 'LI') return;
-//         document.execCommand('formatBlock', false, 'p');
-//     }
-// };
+postBtn.addEventListener('click', function(event) {
+    event.preventDefault;
+    let text = visuellView.textContent();
+    console.log(text);
+    console.log(textTwo);
+})
